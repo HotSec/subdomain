eb8565eb6a3ea2ce2058bb2c5b81bee0bbfb22dc
Gitea <gitea@fake.local>
2022-02-21 08:49:16 +0000

    🎉 init(init): 初始化项目

---
+++ subdomain/subdomain.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+'''
+Version: 0.1
+Autor: zmf96
+Email: zmf96@qq.com
+Date: 2022-02-21 08:46:14
+LastEditors: zmf96
+LastEditTime: 2022-02-21 08:48:31
+FilePath: /subdomain/subdomain.py
+Description: 
+'''
+from loguru import logger
+
+def main():
+    logger.debug("subdomain  version 0.1")
+    pass
+
+if __name__ == '__main__':
+    main()
